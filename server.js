727b6ee61e8fe786dcf3785613c602e12af2f04e
Yeasir H <yeasir01@gmail.com>
2020-04-27 02:12:24 -0700

    refactored code

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes/api.js");
-const PORT = process.env.PORT || 3000;
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,11 +10,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+  useNewUrlParser: true
 });
 
-app.use(routes);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("==> Listening on port %s. Visit http://localhost:%s in your browser", PORT, PORT)
