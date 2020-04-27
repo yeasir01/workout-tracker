56bc007f141ddf6f9505a2d3925627bcd00c0ff8
Yeasir H <yeasir01@gmail.com>
2020-04-27 02:13:08 -0700

    added distance object

--- models/workout.js
+++ models/workout.js
@@ -1,34 +1,41 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Enter workout type"
+  day: {
+    type: Date,
+    default: Date.now
   },
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter name of workout"
-  },
-  duration: {
-    type: Number,
-    default: 0
-  },
-  weight: {
-    type: Number,
-    default: 0
-  },
-  reps: {
-    type: Number,
-    default: 0
-  },
-  sets: {
-    type: Number,
-    default: 0
-  }
+  exercises: [{
+    type: {
+      type: String,
+      trim: true
+    },
+    name: {
+      type: String,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      default: 0
+    },
+    weight: {
+      type: Number,
+      default: 0
+    },
+    reps: {
+      type: Number,
+      default: 0
+    },
+    sets: {
+      type: Number,
+      default: 0
+    },
+    distance: {
+      type: Number,
+      default: 0
+    }
+  }]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
