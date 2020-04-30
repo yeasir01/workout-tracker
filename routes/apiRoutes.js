1cf983778f788c479bad2c55df510c8242d2ab9e
Yeasir H <yeasir01@gmail.com>
2020-04-29 19:20:21 -0700

    setup new routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,38 @@
 const Workout = require("../models/workout.js");
 
 module.exports = function (app) {
-  app.get("/api/workout", (req, res) => {
+
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+        .then(dbWorkout => {
+          res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
     Workout.find({})
       .then(dbWorkout => {
         res.json(dbWorkout);
