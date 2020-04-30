1798539711cd51ed1032fc331a6eafdca27d0620
Yeasir H <yeasir01@gmail.com>
2020-04-30 13:14:13 -0700

    required morgan for testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(morgan("tiny"))
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
