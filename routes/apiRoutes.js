6ca5e934ad2981be5099b98e949fa49d4ef49e18
Yeasir H <yeasir01@gmail.com>
2020-04-30 13:14:37 -0700

    finished api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,14 +12,14 @@ module.exports = function (app) {
       });
   });
 
-  app.post("/api/workouts", ({body}, res) => {
-    Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
+  app.post('/api/workouts', (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+          res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
   });
 
   app.put('/api/workouts/:id', (req, res) => {
